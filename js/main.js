dcac2f55a5ef19997f92bcfbd64f32fab7f36913
Charles Lennon <charleslcsantos@gmail.com>
2014-05-23 20:04:26 -0300

    produtos em andamento [Fiscal - OK]

--- js/main.js
+++ js/main.js
@@ -37,6 +37,11 @@ function hasScrolled() {
     lastScrollTop = st;
 }
 
+var produto1 = document.getElementById("produto1"),
+    produto2 = document.getElementById("produto2"),
+    produto3 = document.getElementById("produto3"),
+    produto4 = document.getElementById("produto4");
+
 function valida(){
 
     var nome = document.getElementById("nome");
@@ -44,9 +49,38 @@ function valida(){
     var mensagem = document.getElementById("mensagem");
 
     if((nome.value == "") || (email.value == "") || (mensagem.value == "")){
-        alert("Preencha os campos obrigatórios");
+        alert("Preencha os campos obrigatórios (*)");
     } else {
         document.formulario.submit();
     }
 
+}
+
+function viewProduct(arg){
+    $('#produto1').removeClass("fade in").addClass('fade out ocultar');
+    $('#produto2').removeClass("fade in").addClass('fade out ocultar');
+    $('#produto3').removeClass("fade in").addClass('fade out ocultar');
+    $('#produto4').removeClass("fade in").addClass('fade out ocultar');
+
+    $('#'+arg).removeClass('fade out ocultar').addClass("fade in");
+    closeItens();
+}
+
+function closeItens() {
+    var elements = document.querySelectorAll("div.item"), i;
+    for (i=0; i<elements.length; i++) {
+        viewItem(elements[i]);
+    }
+}
+
+function viewItem (arg) {
+    if(hasClass(arg, 'over')===false) {
+        $(arg).addClass('over');
+    }else {
+        $(arg).removeClass('over');
+    }
+}
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
